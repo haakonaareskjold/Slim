bfa929e9cef08d6ac11cbb383ce3e7f3bfc22d73
Haakon <aareskjold.haakon@gmail.com>
2020-05-28 01:21:41 +0200

    added autoloading

--- app/routes.php
+++ app/routes.php
@@ -1,18 +1,9 @@
 <?php
 
 use Slim\App;
-use Jenssegers\Blade\Blade;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 return function (App $app) {
-    function view(Response $response, $template, $with=[]) {
-        $cache = __DIR__. '/../cache';
-        $views = __DIR__ . '/../resources/views';
-
-        $blade = (new Blade($views, $cache))->make($template, $with);
-        $response->getBody()->write($blade->render());
-        return $response;
-    };
 
     $app->get('/home', function (Request $request, Response $response, $parameters) {
         $name = 'Haakon';
