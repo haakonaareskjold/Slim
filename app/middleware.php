11ff73923852e292defe6a0642b8ff4c1da12252
Haakon36 <aareskjold.haakon@gmail.com>
2020-05-27 14:01:59 +0200

    added directories and files for project

---
+++ app/middleware.php
@@ -0,0 +1,14 @@
+<?php
+
+use Slim\App;
+
+return function (App $app) {
+    $setting = $app->getContainer()->get('settings');
+
+    $app->addErrorMiddleware(true, true, true);
+    $app->addErrorMiddleware(
+        $setting['displayErrorDetails'],
+        $setting['logErrors'],
+        $setting['logErrorDetails']
+    );
+};
---
+++ cache/51579e624a8cb58bee5c649f925487ae5698d972.php
@@ -0,0 +1,8 @@
+<?php $__env->startSection('content'); ?>
+    <div style="padding:10px">
+        <p>
+            <?php echo e($name); ?> Auth Home Page
+        </p>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/haakon/PhpstormProjects/authorize-slim-4/resources/views/auth/home.blade.php ENDPATH**/ ?>
