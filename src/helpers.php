83890e838dd44bd077c07ff5d784babd1ec0d150
Haakon <aareskjold.haakon@gmail.com>
2021-01-04 22:06:45 +0100

    refactored directorie namies

--- src/helpers.php
+++ src/helpers.php
@@ -7,7 +7,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 if (!function_exists('view')) {
     function view(Response $response, $template, $with = [])
     {
-        $cache = __DIR__ . '/../cache';
+        $cache = __DIR__ . '/../storage/views';
         $views = __DIR__ . '/../resources/views';
 
         $blade = (new Blade($views, $cache))->make($template, $with);
