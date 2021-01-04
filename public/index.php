80cdaa8e4f7aa13a5165498273eac2a8a00c6041
Haakon <aareskjold.haakon@gmail.com>
2021-01-04 16:11:47 +0100

    updated composer.lock and versions

--- public/index.php
+++ public/index.php
@@ -1,3 +1,23 @@
 <?php
 
-require '../bootstrap/index.php';
+use DI\Container;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+$container = new Container();
+
+$settings = require __DIR__ . '/../src/settings.php';
+$settings($container);
+
+$app = \Slim\Factory\AppFactory::create($container);
+
+
+$middleware = require __DIR__ . '/../src/middleware.php';
+$middleware($app);
+
+$routes = require __DIR__ . '/../src/routes.php';
+$routes($app);
+
+$app->run();
+
