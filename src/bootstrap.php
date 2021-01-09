03ccdfa267a134e70a53902b191cd5b43f1351b5
Haakon <aareskjold.haakon@gmail.com>
2021-01-09 18:47:49 +0100

    refactored settings back and entitymanager in own file

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,36 +2,19 @@
 
 use DI\Container;
 use DI\Bridge\Slim\Bridge as AppFactory;
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
-
 
 require_once __DIR__. '/../vendor/autoload.php';
 
 
 $container = new Container();
 
+// Set up settings
+$settings = require __DIR__ . '/../src/settings.php';
+$settings($container);
 
-$entitymanagerFactory = function () {
-// Create a simple "default" Doctrine ORM configuration for Annotations
-    $isDevMode = true;
-    $proxyDir = null;
-    $cache = null;
-    $useSimpleAnnotationReader = false;
-    $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ ), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
-// or if you prefer yaml or XML
-//$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
-//$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
+$entitymanagerFactory = require __DIR__ . '/../src/entitymanager.php';
+$entitymanagerFactory($container);
 
-// database configuration parameters
-    $conn = array(
-        'driver' => 'pdo_sqlite',
-        'path' => __DIR__ . '/db.sqlite',
-    );
-
-// obtaining the entity manager
-    return EntityManager::create($conn, $config);
-};
 $containerBuilder = new \DI\ContainerBuilder();
 $containerBuilder->useAutowiring(true);
 
---
+++ src/entitymanager.php
@@ -0,0 +1,26 @@
+<?php
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+
+return function () {
+// Create a simple "default" Doctrine ORM configuration for Annotations
+    $isDevMode = true;
+    $proxyDir = null;
+    $cache = null;
+    $useSimpleAnnotationReader = false;
+    $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ ), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
+// or if you prefer yaml or XML
+//$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
+//$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
+
+// database configuration parameters
+    $conn = array(
+        'driver' => 'pdo_sqlite',
+        'path' => __DIR__ . '/db.sqlite',
+    );
+
+// obtaining the entity manager
+    return $entitymanagerFactory = EntityManager::create($conn, $config);
+
+};
